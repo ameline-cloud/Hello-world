5d75460586c299ad14f5784cd2cd8c113f43bdc8
ameline-cloud <amelinekamg@gmail.com>
2020-07-17 20:43:48 -0400

    updated variable projet

---
+++ VariableProjet.java
@@ -0,0 +1,17 @@
+public class VariableProjet {
+
+    public static void main(String[] args) {
+    	
+    	char ch = 'X';
+    	System.out.println(ch);
+
+	String str = "LearningTest";
+	System.out.println(str);
+
+        int num1=50;
+        num1++;
+        System.out.println("num1++ is: "+num1);
+      
+  
+    }
+} 
