eb8f9c09667a329f34b97c8f0b224b543e41df34
ameline-cloud <amelinekamg@gmail.com>
2020-07-17 21:13:03 -0400

    Continue Praticise

---
+++ variablesproject.java
@@ -0,0 +1,15 @@
+public class variablesproject {
+
+    public static void main(String[] args) {
+int data=50;//instance variable 
+int x=100;//static variable  
+String str = "LearningTest";//class variable
+char ch = 'p';//char variable
+
+    	System.out.println(data);
+        System.out.println(x);
+        System.out.println(ch);
+        System.out.println(str);
+    }
+}
+
